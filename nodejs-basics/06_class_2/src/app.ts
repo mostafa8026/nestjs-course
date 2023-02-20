eed3cdc9afa641093bee761fb30f75ac3435f8e8
mostafa8026 <mostafa8026@gmail.com>
2023-02-20 21:55:50 +0330

    fix netlify problem

--- nodejs-basics/06_class_2/src/app.ts
+++ nodejs-basics/06_class_2/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World');
 });
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
