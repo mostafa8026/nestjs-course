5bf1fbdacc250a1d779ae2ec65c1b44eeab6b4de
mostafa8026 <mostafa8026@gmail.com>
2023-02-20 23:05:10 +0330

    fix: bring back port to 3000

--- nodejs-basics/06_class_2/src/app.ts
+++ nodejs-basics/06_class_2/src/app.ts
@@ -9,6 +9,6 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World');
 });
 
-app.listen(80, () => {
-  console.log('Server is running on port 80');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
