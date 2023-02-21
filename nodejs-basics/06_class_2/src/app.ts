1f1d3ed25e2e0aa82ec135ad1e0b53c946b11a85
mostafa <mostafa8026@gmail.com>
2023-02-21 08:25:07 +0330

    fix: change port to 443

--- nodejs-basics/06_class_2/src/app.ts
+++ nodejs-basics/06_class_2/src/app.ts
@@ -9,6 +9,6 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(443, () => {
+  console.log('Server is running on port 443');
 });
