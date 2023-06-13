441c6f373c59c597b7955bff841efb0fc07dadb1
mostafa <mostafa8026@gmail.com>
2023-06-13 07:05:14 +0330

    fix: remove what should be removed

--- nestjs/06/translator.removed/src/json-storage/json-storage.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JsonStorageService } from './json-storage.service';
-
-@Module({
-  providers:[JsonStorageService],
-  exports: [JsonStorageService],
-})
-export class JsonStorageModule {}
